85dda638fe14690c7b6a8abd64fcd1b345f45955
bhaveshpadharia <padharia.bhavesh06@gmail.com>
2019-05-28 13:13:32 +0530

    arithmetical operations

---
+++ arithmetical operations.py
@@ -0,0 +1,14 @@
+#store the input values
+
+num1 = input('enter the first number')
+num2 = input('enter the second number')
+
+#add two numbers
+
+sum = float(num1) + float(num2)
+
+#display the sum
+
+print('the sum of {0} and {1} is {2}'.format(num1,num2,sum))
+
+
