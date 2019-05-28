b0306a7ae5008697d138728e2d452cf649ffd6ac
bhaveshpadharia <padharia.bhavesh06@gmail.com>
2019-05-28 14:49:09 +0530

    arithmetical operations

--- arithmetical operations.py
+++ arithmetical operations.py
@@ -1,14 +1,30 @@
 #store the input values
 
-num1 = input('enter the first number')
-num2 = input('enter the second number')
+num1 = input('enter the first number:')
+num2 = input('enter the second number:')
 
 #add two numbers
 
 sum = float(num1) + float(num2)
 
-#display the sum
+#subtract two numbers
+
+min = float(num1) - float(num2)
+
+#multiply two numbers
+
+mul = float(num1) * float(num2)
+
+#divide two numbers
+
+div = float(num1) / float(num2)
+
+#display the result
 
 print('the sum of {0} and {1} is {2}'.format(num1,num2,sum))
+print('the min of {0} and {1} is {2}'.format(num1,num2,min))
+print('the mul of {0} and {1} is {2}'.format(num1,num2,mul))
+print('the div of {0} and {1} is {2}'.format(num1,num2,div))
+
 
 
